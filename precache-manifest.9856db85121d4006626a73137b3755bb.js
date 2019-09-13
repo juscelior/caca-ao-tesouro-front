407dd36e04fa84633d26a54e602d313ec99c4d91
Juscélio Reis <juscelioreis@wizsolucoes.com.br>
2019-09-13 14:29:14 -0300

    Updates

---
+++ precache-manifest.9856db85121d4006626a73137b3755bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479d353adda8e50073c482e6c2e39ee7",
+    "url": "/caca-ao-tesouro-front/index.html"
+  },
+  {
+    "revision": "c3f70c1a1561e3c9eb16",
+    "url": "/caca-ao-tesouro-front/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a5d7de5e916073a8e735",
+    "url": "/caca-ao-tesouro-front/static/js/2.acfdbf5a.chunk.js"
+  },
+  {
+    "revision": "c3f70c1a1561e3c9eb16",
+    "url": "/caca-ao-tesouro-front/static/js/main.7ad3d06b.chunk.js"
+  },
+  {
+    "revision": "5c6cc38af24054f7af2f",
+    "url": "/caca-ao-tesouro-front/static/js/runtime~main.d45626b3.js"
+  }
+]);
